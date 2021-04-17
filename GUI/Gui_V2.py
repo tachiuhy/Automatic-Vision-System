af8ce1b5ff426f74f8f9cf1d0fc4b230e90d714f
KhangLeNguyen <lenguyenankhang07@gmail.com>
2021-04-17 18:21:02 +0700

    New Updates   Label objects were replaced graphic views   setPhoto() function to show images in 3 label Fault   Bottle Counter: 3 image

--- GUI/Gui_V2.py
+++ GUI/Gui_V2.py
@@ -1,6 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
 import mainfile
+import firstsetup as FST
+import cv2
+import imutils as imu
 
 
 class Ui_MainWindow(object):
@@ -19,21 +22,33 @@ class Ui_MainWindow(object):
         self.pushButton_Stop = QtWidgets.QPushButton(self.tab_Overview)
         self.pushButton_Stop.setGeometry(QtCore.QRect(540, 40, 75, 23))
         self.pushButton_Stop.setObjectName("pushButton_Stop")
-        self.graphicsView_Mode2 = QtWidgets.QGraphicsView(self.tab_Overview)
-        self.graphicsView_Mode2.setGeometry(QtCore.QRect(280, 0, 256, 192))
-        self.graphicsView_Mode2.setObjectName("graphicsView_Mode2")
+
         self.Line_Edit_Path = QtWidgets.QLineEdit(self.tab_Overview)
         self.Line_Edit_Path.setGeometry(QtCore.QRect(280, 370, 171, 20))
         self.Line_Edit_Path.setObjectName("Line_Edit_Path")
-        self.graphicsView_Mode1 = QtWidgets.QGraphicsView(self.tab_Overview)
+
+        self.graphicsView_Mode1 = QtWidgets.QLabel(self.tab_Overview)
         self.graphicsView_Mode1.setGeometry(QtCore.QRect(20, 0, 256, 192))
+        self.graphicsView_Mode1.setFrameShape(QtWidgets.QFrame.Panel)
+        self.graphicsView_Mode1.setFrameShadow(QtWidgets.QFrame.Plain)
         self.graphicsView_Mode1.setObjectName("graphicsView_Mode1")
-        self.graphicsView_Mode3 = QtWidgets.QGraphicsView(self.tab_Overview)
+
+        self.graphicsView_Mode2 = QtWidgets.QLabel(self.tab_Overview)
+        self.graphicsView_Mode2.setGeometry(QtCore.QRect(280, 0, 256, 192))
+        self.graphicsView_Mode2.setFrameShape(QtWidgets.QFrame.Panel)
+        self.graphicsView_Mode2.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.graphicsView_Mode2.setObjectName("graphicsView_Mode2")
+
+        self.graphicsView_Mode3 = QtWidgets.QLabel(self.tab_Overview)
         self.graphicsView_Mode3.setGeometry(QtCore.QRect(20, 200, 256, 192))
+        self.graphicsView_Mode3.setFrameShape(QtWidgets.QFrame.Panel)
+        self.graphicsView_Mode3.setFrameShadow(QtWidgets.QFrame.Plain)
         self.graphicsView_Mode3.setObjectName("graphicsView_Mode3")
+
         self.Button_Start = QtWidgets.QPushButton(self.tab_Overview)
         self.Button_Start.setGeometry(QtCore.QRect(540, 10, 75, 23))
         self.Button_Start.setObjectName("Button_Start")
+
         self.textBrowser = QtWidgets.QTextBrowser(self.tab_Overview)
         self.textBrowser.setGeometry(QtCore.QRect(280, 200, 256, 161))
         self.textBrowser.setObjectName("textBrowser")
@@ -106,6 +121,26 @@ class Ui_MainWindow(object):
 
     def printConsole(self, result):
         self.textBrowser.append(result)
+    def setPhoto(self, processImg1, processImg2, processImg3):
+        img1_processed = imu.resize(processImg1, width=600)
+        frame1 = cv2.cvtColor(img1_processed, cv2.COLOR_BGR2RGB)
+        cv2.waitKey(1000)
+        img1_processed = QImage(frame1, frame1.shape[1], frame1.shape[0], frame1.strides[0], QImage.Format_RGB888)
+        self.graphicsView_Mode1.setPixmap(QPixmap.fromImage(img1_processed))
+
+        img2_processed = imu.resize(processImg2, width=600)
+        frame2 = cv2.cvtColor(img2_processed, cv2.COLOR_BGR2RGB)
+        cv2.waitKey(1000)
+        img2_processed = QImage(frame2, frame2.shape[1], frame2.shape[0], frame2.strides[0], QImage.Format_RGB888)
+        self.graphicsView_Mode1.setPixmap(QPixmap.fromImage(img2_processed))
+
+        img3_processed = imu.resize(processImg3, width=600)
+        frame3 = cv2.cvtColor(img3_processed, cv2.COLOR_BGR2RGB)
+        cv2.waitKey(1000)
+        img3_processed = QImage(frame3, frame3.shape[1], frame3.shape[0], frame3.strides[0], QImage.Format_RGB888)
+        self.graphicsView_Mode1.setPixmap(QPixmap.fromImage(img3_processed))
+
+
 
 if __name__ == "__main__":
     import sys
